44df86b1f843ce3f33e88fa8ce59e3d58809ba17
Ana Robalo <anameciarobalo@gmail.com>
2019-11-13 16:19:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   new_array = []
-  %w(string).each { |s| new_array << s.reverse} 
+  string.split.each { |s| new_array << s.reverse} 
   new_string = new_array.join(" ") 
   new_string
 end
